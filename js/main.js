2a18875f465b16b27679c6178ad63df4729e71ca
iamtushar <ecetushar22@gmail.com>
2022-11-04 10:15:21 +0530

    adds login system

--- js/main.js
+++ js/main.js
@@ -6,3 +6,25 @@ function showPassword() {
       x.type = "password";
     }
   }
+
+function login(){
+  var username = document.getElementById("usernameBox").value;
+  var pwd = document.getElementById("passwordBox").value;
+  console.log(username);
+  console.log(pwd)
+  
+  let payload = {"email":username, "password":pwd};
+  console.log(payload);
+  fetch("http://192.168.18.129:5000/user/login/",{
+    method:"POST",
+    headers:{Accept: "application.json","Content-Type": "application/json"},
+    body:JSON.stringify(payload),
+    cache:"default"
+  }).then(res => {
+    console.log("response:", res);
+  });
+  
+  
+
+
+}
