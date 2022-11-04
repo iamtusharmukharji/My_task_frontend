d956e10e4cf857a24d4153fbedb6a227fbdc24be
Tushar Mukahrji <ecetushar22@gmail.com>
2022-11-04 16:13:10 +0530

    adds login and signup functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+const url = "http://192.168.18.129:5000";
+
 function showPassword() {
     var x = document.getElementById("passwordBox");
     if (x.type === "password") {
@@ -9,24 +11,86 @@ function showPassword() {
     }
   }
 
-function login(){
+
+async function login(){
   var username = document.getElementById("usernameBox").value;
   var pwd = document.getElementById("passwordBox").value;
-  console.log(username);
-  console.log(pwd)
   
   let payload = {"email":username, "password":pwd};
   console.log(payload);
-  fetch("http://192.168.18.129:5000/user/login/",{
+
+  const response = await fetch(url+"/user/login/",{
     method:"POST",
     headers:{Accept: "application.json","Content-Type": "application/json"},
     body:JSON.stringify(payload),
     cache:"default"
-  }).then(res => {
-    console.log("response:", res);
   });
+
+  var data = await response.json();
+  console.log(data);
+  console.log(response.status);
+  const jwt_token = data.token;
+  localStorage.setItem("token", jwt_token);
+  //window.location.href="task-home.html";
+}
+
+async function signup(){
+  var email = document.getElementById("emailBox").value;
+  var name = document.getElementById("nameBox").value;
+  var password = document.getElementById("passBox").value;
+  var check = document.getElementById("cnfPassBox").value;
+  var alert = document.getElementById("alert");
+  var button = document.getElementById("create-acc-button");
+  var loader = document.getElementById("loader");
+
+  button.style.display = "none";
+  loader.style.display = "block";
+
+  if (password!=check){
+    loader.style.display = "none";
+    button.style.display = "block";
+  alert.innerHTML = "Password not matched!";
+  console.log("password not matched");
+  return;
+  }
   
+  let payload = {"email":email, "name":name, "password":password};
+
+  const resp = await fetch(url+"/user/signup/",
+  {
+    method:"POST",
+    headers:{Accept: "application.json", "Content-Type": "application/json"},
+    body:JSON.stringify(payload),
+    cache:"default"
+  });
+  if (resp.status == 400){
+    alert.innerHTML = "User already exists";
+    console.log("password not matched");
+    
+    loader.style.display = "none";
+    button.style.display = "block";
+    return;
+  }
+  var data = await resp.json();
+  console.log(data);
+  showModal("form-modal",0);
+  button.style.display = "block";
+  loader.style.display = "none";
   
+}
 
 
+function showModal(id,flag){
+  var modal = document.getElementById(id)
+  document.getElementById("emailBox").value = '';  
+  document.getElementById("nameBox").value = '';
+  document.getElementById("passBox").value = '';
+  document.getElementById("cnfPassBox").value = '';
+  document.getElementById("alert").innerHTML = '';
+  if (flag == 1){
+    modal.style.display = "block";
+  }
+  else{
+    modal.style.display = "none";
+  }
 }
